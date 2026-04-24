69fdf59720371a15f90ba737af4d121ec085065e
ethan <e3thanp@my.yorku.ca>
2026-04-24 03:50:23 -0400

    fix cors

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || '*' }));
+app.use(cors());
 
 app.post('/analyze', async (req, res) => {
   const { system_description, framework } = req.body;
